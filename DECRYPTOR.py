188fa7ce599484ec2cd5b97277f4c28e2b838938
Ella <ellacalugay@gmail.com>
2023-04-15 22:05:28 +0800

    Pseudocode

--- DECRYPTOR.py
+++ DECRYPTOR.py
@@ -1,1 +1,11 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #2 | PROBLEM 2 - DECRYPTION
+
+#Pseudocode
+# Ask the user to provide an encrypted text to be decrypted.
+# Go through each character in the string.
+    #   If the symbol is asterisk "*", replace it with the character "a"
+    #   If the symbol is ampersand "&", replace it with the character "e"
+    #   If the symbol is hash "#", replace it with the character "i"
+    #   If the symbol is plus "+", replace it with the character "o"
+    #   If the symbol is exclamation point "!", replace it with the character "u"
+# Print the output.
