034b4b098d416ef8989b56ea98722d463dd8d60a
Ella <ellacalugay@gmail.com>
2023-04-15 22:15:06 +0800

    Characters are working really grate! Hooraaaygit add -A

--- DECRYPTOR.py
+++ DECRYPTOR.py
@@ -2,10 +2,27 @@
 
 #Pseudocode
 # Ask the user to provide an encrypted text to be decrypted.
+user_input_string = input("Enter a string to decrypt: ")
+output_str = ""
 # Go through each character in the string.
-    #   If the symbol is asterisk "*", replace it with the character "a"
-    #   If the symbol is ampersand "&", replace it with the character "e"
-    #   If the symbol is hash "#", replace it with the character "i"
-    #   If the symbol is plus "+", replace it with the character "o"
-    #   If the symbol is exclamation point "!", replace it with the character "u"
+for char in range(len(user_input_string)): 
+#   If the symbol is asterisk "*", replace it with the character "a"
+    if user_input_string[char] == "*":
+        output_str += "a" 
+#   If the symbol is ampersand "&", replace it with the character "e"
+    elif user_input_string[char] == "&":
+        output_str += "e"
+#   If the symbol is hash "#", replace it with the character "i"
+    elif user_input_string[char] == "#":
+        output_str += "i"
+#   If the symbol is plus "+", replace it with the character "o"
+    elif user_input_string[char] == "+":
+        output_str += "o"
+#   If the symbol is exclamation point "!", replace it with the character "u"
+    elif user_input_string[char] == "!":
+        output_str += "u"
+    else:
+        output_str += user_input_string[char]
+
 # Print the output.
+print (output_str)
