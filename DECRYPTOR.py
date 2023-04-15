11e23f0a92b120255afe1f262c21ed7a59cca88b
Ella <ellacalugay@gmail.com>
2023-04-15 22:42:01 +0800

    Adding forecolor and background color to the user inputs

--- DECRYPTOR.py
+++ DECRYPTOR.py
@@ -14,9 +14,11 @@ print("\033[38;5;218m" + """
 """ + "\033[0m")
 
 #Pseudocode
-# Ask the user to provide an encrypted text to be decrypted.
-user_input_string = input("Enter a string to decrypt: ")
+
+# Ask the user to provide an encrypted text to be decrypted with some foreground colors and background colors.
+user_input_string = input("\033[38;5;139;1m\033[48;5;225mEnter a string to decrypt:\033[0m \033[34m")
 output_str = ""
+
 # Go through each character in the string.
 for char in range(len(user_input_string)): 
 #   If the symbol is asterisk "*", replace it with the character "a"
